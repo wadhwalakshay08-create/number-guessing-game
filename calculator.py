881adec6632958a1c5754ac58f7a2f6cfccf7ff8
wadhwalakshay08-create <wadhwalakshay08@gmail.com>
2026-09-04 10:55:06 +0530

    Simple Calculator from Python

---
+++ calculator.py
@@ -0,0 +1,29 @@
+print("==============================")
+print("      PYTHON CALCULATOR")
+print("==============================")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif operator == "-":
+    result = num1 - num2
+    print("Result:", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("Result:", result)
+
+elif operator == "/":
+    if num2 == 0:
+        print("Cannot divide by zero.")
+    else:
+        result = num1 / num2
+        print("Result:", result)
+
+else:
+    print("Invalid operator.")
